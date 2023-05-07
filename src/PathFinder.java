d214f03ec41b9a08e2e0a021b2ba15232e208b4d
Kingomac <31375644+Kingomac@users.noreply.github.com>
2023-05-07 16:47:09 +0200

    Correcciones en el algoritmo

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -3,20 +3,20 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Arrays;
+import java.util.Collections;
 
 import jason.environment.grid.Location;
+import jason.stdlib.list;
 
 public class PathFinder {
-
-  public final int INT_MAX = Integer.MAX_VALUE;
 
   public class Celda {
     public int f, h, g;
     public int padre_i, padre_j;
 
     public Celda() {
-      this.f = INT_MAX;
-      this.h = INT_MAX;
+      this.f = Integer.MAX_VALUE;
+      this.h = Integer.MAX_VALUE;
       this.g = 0;
       this.padre_i = -1;
       this.padre_j = -1;
@@ -24,8 +24,8 @@ public class PathFinder {
 
     public Celda(int f, int h, int g, int padre_i, int padre_j) {
       this.f = f;
-      this.h = g;
-      this.g = h;
+      this.h = h;
+      this.g = g;
       this.padre_i = padre_i;
       this.padre_j = padre_j;
     }
@@ -43,27 +43,14 @@ public class PathFinder {
   }
 
   public Location getDirection(Location origen, Location destino, SpecializedRobots me) {
-    System.out.println("----- getDirection");
-    int[][] matrizAdyacencia = new int[HouseModel.GSize][HouseModel.GSize];
 
-    for (int i = 0; i < model.getHeight(); i++) {
-      for (int j = 0; j < model.getWidth(); j++) {
-        matrizAdyacencia[i][j] = model.isFreeOfObstacle(j, i) ? 0 : 1;
-      }
-    }
-
-    System.out.println("----- Matriz de adyacencia");
-    for (int[] i : matrizAdyacencia) {
-      System.out.println(Arrays.toString(i));
-    }
-
-    Celda[][] infoMatriz = new Celda[HouseModel.GSize][HouseModel.GSize];
+    Celda[][] infoMatriz = new Celda[model.getHeight()][model.getWidth()];
     for (int i = 0; i < infoMatriz.length; i++) {
-      for (int j = 0; j < infoMatriz.length; j++) {
+      for (int j = 0; j < infoMatriz[i].length; j++) {
         infoMatriz[i][j] = new Celda();
         infoMatriz[i][j].h = new Location(j, i).distanceManhattan(destino);
-        if (!model.isFreeOfObstacle(j, i)) {
-          infoMatriz[i][j].g = 1000;
+        if (!model.isFreeOfObstacle(i, j)) {
+          infoMatriz[i][j].g = Integer.MAX_VALUE;
         }
       }
     }
@@ -77,14 +64,12 @@ public class PathFinder {
     List<Location> listaCerrada = new LinkedList<>();
     listaAbierta.add(origen);
     infoMatriz[origen.y][origen.x] = new Celda(0, 0, 0, origen.y, origen.x);
-    // f = h + g --> f = distanciaManhattan(nodo,destino) + g(si hay camino 0
-    // INT_MAX)
 
     boolean caminoEncontrado = false;
     while (!listaAbierta.isEmpty() && !caminoEncontrado) {
       System.out.println("Lista abierta: " + listaAbierta.toString());
       System.out.println("Lista cerrada: " + listaCerrada.toString());
-      Location q = getMinF(listaAbierta, infoMatriz);
+      Location q = getMinF(listaAbierta, listaCerrada, infoMatriz);
       listaAbierta.remove(q);
       listaCerrada.add(q);
 
@@ -101,7 +86,7 @@ public class PathFinder {
           infoMatriz[destino.y][destino.x].padre_i = q.y;
           infoMatriz[destino.y][destino.x].padre_j = q.x;
           List<Location> resultado = getResultado(infoMatriz, origen, destino);
-          System.out.println("RESULTADOOOOOOOOOOOOOOOOOO");
+          System.out.println("RESULTADO");
           System.out.println(resultado.toString());
           return resultado.get(0);
           ///////////////////////////////////// return getResultado(infoMatriz, destino);
@@ -112,7 +97,7 @@ public class PathFinder {
           int fNew = infoMatriz[next.y][next.x].h + gNew;
           if (!listaAbierta.contains(next) || infoMatriz[next.y][next.x].f > fNew) {
             listaAbierta.add(next);
-            infoMatriz[next.y][next.x].f = gNew;
+            infoMatriz[next.y][next.x].g = gNew;
             infoMatriz[next.y][next.x].f = fNew;
             infoMatriz[next.y][next.x].padre_i = q.y;
             infoMatriz[next.y][next.x].padre_j = q.x;
@@ -126,11 +111,11 @@ public class PathFinder {
     return null;
   }
 
-  Location getMinF(List<Location> listaAbierta, Celda[][] infoMatriz) {
-    int minF = INT_MAX;
+  Location getMinF(List<Location> listaAbierta, List<Location> listaCerrada, Celda[][] infoMatriz) {
+    int minF = Integer.MAX_VALUE;
     Location res = null;
     for (Location i : listaAbierta) {
-      if (infoMatriz[i.y][i.x].f < minF) {
+      if (!listaCerrada.contains(i) && infoMatriz[i.y][i.x].f < minF) {
         minF = infoMatriz[i.y][i.x].f;
         res = i;
       }
@@ -139,27 +124,8 @@ public class PathFinder {
   }
 
   List<Location> getResultado(Celda[][] infoMatriz, Location orig, Location dest) {
-    System.out.println("-- get resultado");
-    System.out.println("----- infoMatriz");
-    int i = 0;
-    int j = 0;
-    /*
-     * for (Celda[] i2 : infoMatriz) {
-     * j = 0;
-     * 
-     * for (Celda j2 : i2) {
-     * if (j2.padre_i != -1) {
-     * System.out.print("(" + j + "," + i + ")" + j2 + " | ");
-     * }
-     * j++;
-     * }
-     * i++;
-     * 
-     * }
-     */
-
-    /* int */i = dest.y;
-    /* int */j = dest.x;
+    int i = dest.y;
+    int j = dest.x;
     System.out.println("aaaaaaaaaaaaa");
     List<Location> resultado = new LinkedList<>();
     while (!(infoMatriz[i][j].padre_i == i && infoMatriz[i][j].padre_j == j)) {
@@ -172,18 +138,26 @@ public class PathFinder {
       j = newj;
     }
 
+    Collections.reverse(resultado);
     return resultado;
   }
 
+  /**
+   * Devuelve las posiciones adyacentes a una casilla si están libres siguiendo un
+   * movmiento Manhattan
+   * 
+   * @param pos
+   * @return
+   */
   List<Location> getNextLocations(Location pos) {
     List<Location> toret = new LinkedList<>();
-    if (model.inGrid(pos.x + 1, pos.y) && model.isFree(pos.x + 1, pos.y))
+    if (model.inGrid(pos.x + 1, pos.y) && model.isFreeOfObstacle(pos.x + 1, pos.y))
       toret.add(new Location(pos.x + 1, pos.y));
-    if (model.inGrid(pos.x - 1, pos.y) && model.isFree(pos.x - 1, pos.y))
+    if (model.inGrid(pos.x - 1, pos.y) && model.isFreeOfObstacle(pos.x - 1, pos.y))
       toret.add(new Location(pos.x - 1, pos.y));
-    if (model.inGrid(pos.x, pos.y - 1) && model.isFree(pos.x, pos.y - 1))
+    if (model.inGrid(pos.x, pos.y - 1) && model.isFreeOfObstacle(pos.x, pos.y - 1))
       toret.add(new Location(pos.x, pos.y - 1));
-    if (model.inGrid(pos.x, pos.y + 1) && model.isFree(pos.x, pos.y + 1))
+    if (model.inGrid(pos.x, pos.y + 1) && model.isFreeOfObstacle(pos.x, pos.y + 1))
       toret.add(new Location(pos.x, pos.y + 1));
 
     return toret;
